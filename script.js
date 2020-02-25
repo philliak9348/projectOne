29e14ca769675ac8d8b4666a838c4ae492e9a69a
phill <philliak9348@uwec.edu>
2020-02-25 11:35:14 -0600

    push5

---
+++ script.js
@@ -0,0 +1,10 @@
+function openInf(button, div) {
+  if (document.getElementById.style.width != "250.px") {
+  document.getElementById(div).style.width = "250.px";
+  document.getElementsByTagName(body).style.marginLeft = "250.px"
+  }
+  else {
+    document.getElementById(div).style.width = "0.px";
+  document.getElementsByTagName(body).style.marginLeft = "0.px"
+  }
+}
