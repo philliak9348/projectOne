78d8cdb1dbca8e2c4241a22f9973a68714af6f04
phill <philliak9348@uwec.edu>
2020-02-27 19:29:41 -0600

    Push12

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function expButton(ex) {
-  var service = document.getElementById(ex);
-  }
+function expandSection(ex) {
+  var element = document.getElementById(ex);
+  element.classList.toggle("open");
+  element.classList.toggle("close");
+}
 
 function openFile(file) {
   window.open(file);
