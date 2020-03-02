4b446d94d09ee3125afc03ed0005af5af435faa2
philliak9348 <philliak9348@uwec.edu>
2020-03-02 12:18:25 -0600

    pushFromIntellij3

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-function expandSection(ex) {
+function togglePanel(ex) {
   var nav = document.getElementById('nav');
-  var body = document.getElementById('info')
   nav.classList.toggle("panelOpen");
-  body.classList.toggle("infoPart");
   nav.classList.toggle("panelClose");
-  body.classList.toggle("infoFull");
 }
 function openFile(file) {
   window.open(file);
