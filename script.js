55b8e54a9d65d3d13d24b48786a78ca9c429b6cb
phill <philliak9348@uwec.edu>
2020-03-05 11:37:21 -0600

    0305_1127

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ function togglePanel() {
   nav.classList.toggle("openMenu");
   nav.classList.toggle("closedMenu");
 }
+function toggleServices() {
+  var service = document.getElementById('services');
+  var servButton = document.getElementById('servButton');
+  service.classList.toggle("servicesClose");
+  service.classList.toggle("servicesOpen");
+}
 function openFile(file) {
   window.open(file);
 }
